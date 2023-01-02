04d607e9a070302b7dc54573513922b63306c211
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2023-01-02 12:11:07 +0200

    add proptype to details component

--- src/components/Details/details.jsx
+++ src/components/Details/details.jsx
@@ -1,22 +1,67 @@
+import { PropTypes } from 'prop-types';
+
 function Details(props) {
-    return ( <>
-    <div>
+  const {
+    name, revenue, date, period, costOfRevenue, grossProfit, grossProfitRatio, esp,
+  } = props;
+  return (
+    <>
+      <div>
         <img src="" alt="" />
-        <h2>{props.name}</h2>
-        <p>{props.revenue}</p>
-    </div>
-    <div>{props.name} BREAKDOWN - {props.date}</div>
-        <div>
-            <ul>
-            <li><p>period</p> <span>{props.period}</span></li>
-            <li><p>revenue</p> <span>{props.revenue}</span></li>
-            <li><p>costOfRevenue</p> <span>{props.costOfRevenue}</span></li>
-            <li><p>grossProfit</p> <span>{props.grossProfit}</span></li>
-            <li><p>grossProfitRatio</p> <span>{props.grossProfitRatio}</span></li>
-            <li><p>Earnings per share</p> <span>{props.esp}</span></li>
-            </ul>
-        </div>
-    </> );
+        <h2>{name}</h2>
+        <p>{revenue}</p>
+      </div>
+      <div>
+        {name}
+        BREAKDOWN -
+        {date}
+      </div>
+      <div>
+        <ul>
+          <li>
+            <p>period</p>
+            {' '}
+            <span>{period}</span>
+          </li>
+          <li>
+            <p>revenue</p>
+            {' '}
+            <span>{revenue}</span>
+          </li>
+          <li>
+            <p>costOfRevenue</p>
+            {' '}
+            <span>{costOfRevenue}</span>
+          </li>
+          <li>
+            <p>grossProfit</p>
+            {' '}
+            <span>{grossProfit}</span>
+          </li>
+          <li>
+            <p>grossProfitRatio</p>
+            {' '}
+            <span>{grossProfitRatio}</span>
+          </li>
+          <li>
+            <p>Earnings per share</p>
+            {' '}
+            <span>{esp}</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
+Details.propTypes = {
+  name: PropTypes.string.isRequired,
+  revenue: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  period: PropTypes.string.isRequired,
+  costOfRevenue: PropTypes.string.isRequired,
+  grossProfit: PropTypes.string.isRequired,
+  grossProfitRatio: PropTypes.string.isRequired,
+  esp: PropTypes.string.isRequired,
+};
 export default Details;
--- src/components/companies-list/companies-list.jsx
+++ src/components/companies-list/companies-list.jsx
@@ -1,57 +1,61 @@
-import { useEffect } from "react";
+// import { useEffect } from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import img from '../../assets/images/stock-market.jpg';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import './companies.css'
-function CompaniesList() {
-    const companiesLIST = [
-        "ETH",
-        "GOGN",
-        "000540.SZ",
-        "603931.SS",
-        "JMG.L",
-        "067900.KS",
-        "ERM.L",
-        "BSP.AX",
-        "LVCLY",]
+import './companies.css';
 
- const companies = [	{
-    date: "2021-06-30",
-    symbol: "ETH",
-    reportedCurrency: "USD",
-    period: "FY",
+function CompaniesList() {
+//   const companiesLIST = [
+//     'ETH',
+//     'GOGN',
+//     '000540.SZ',
+//     '603931.SS',
+//     'JMG.L',
+//     '067900.KS',
+//     'ERM.L',
+//     'BSP.AX',
+//     'LVCLY'];
+
+  const companies = [{
+    date: '2021-06-30',
+    symbol: 'ETH',
+    reportedCurrency: 'USD',
+    period: 'FY',
     revenue: 685169000,
     costOfRevenue: 292062000,
     grossProfit: 393107000,
     grossProfitRatio: 0.5737372823347232,
     eps: 2.395122340637848,
-}]
-        // useEffect{() => {
-        //     if (companies.length === 10) return
-        //     companiesLIST.forEach(company => {
-        //     dispatch(getValue(company))})
-        // }, []};
-    return ( 
-        <><div className="H-company-container">
-                <img src={img} alt="stock-market" />
-                <span icon="fa-thin fa-money-bill-trend-up" />
-                <h2>{companies[0].symbol}</h2>
-                <span>{companies[0].revenue}</span>
-            </div>
-            <div> <p>US STOCK MARKET</p></div>
-            <div>
-                {
-                    companies.map((company, index) => {
-                        return (
-                            <div key={index}>
-                                <img src={img} alt={company.symbol} />
-                                <h2>{company.symbol}</h2>
-                                <span>{company.revenue}</span>
-                            </div>
-                        )
-                    }
-                    )}
-            </div>
-     </>);
+  }];
+  // useEffect{() => {
+  //     if (companies.length === 10) return
+  //     companiesLIST.forEach(company => {
+  //     dispatch(getValue(company))})
+  // }, []};
+  return (
+    <>
+      <div className="H-company-container">
+        <img src={img} alt="stock-market" />
+        <span icon="fa-thin fa-money-bill-trend-up" />
+        <h2>{companies[0].symbol}</h2>
+        <span>{companies[0].revenue}</span>
+      </div>
+      <div>
+        {' '}
+        <p>US STOCK MARKET</p>
+      </div>
+      <div>
+        {
+                    companies.map((company) => (
+                      <div key={company.symbol}>
+                        <img src={img} alt={company.symbol} />
+                        <h2>{company.symbol}</h2>
+                        <span>{company.revenue}</span>
+                      </div>
+                    ))
+}
+      </div>
+    </>
+  );
 }
 
 export default CompaniesList;
