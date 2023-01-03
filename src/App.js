c0e01a2e173dd4f9b49baa89e0faee42798b1a54
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2023-01-03 14:35:21 +0200

    add api logic and add it to redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import CompaniesPage from './pages/companies-page/Companies-page';
 import DetailsPage from './pages/details-page/details-page';
+import { getValue } from './Redux/companies/companies';
 
+const companiesLIST = [
+  'ETH',
+  'TCRI',
+  'AEF',
+  'FULO',
+  'LVCLY',
+  'ACBA'];
 function App() {
+  const dispatch = useDispatch();
+  const compStore = useSelector((state) => state.companies);
+
+  useEffect(() => {
+    if (compStore.length === 6) return;
+
+    dispatch(getValue(companiesLIST[compStore.length]));
+  }, [dispatch, compStore]);
+
   return (
     <BrowserRouter>
       <Routes>
