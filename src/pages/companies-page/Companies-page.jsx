7942f5a3a9b7adc82da477dbc2c8a21c92e133e2
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2023-01-04 14:13:02 +0200

    update comp-list compnent

--- src/pages/companies-page/Companies-page.jsx
+++ src/pages/companies-page/Companies-page.jsx
@@ -1,5 +1,4 @@
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import CompaniesList from '../../components/companies-list/companies-list';
 import Header from '../../components/header/header';
 import './Companies-page.scss';
@@ -10,22 +9,6 @@ function CompaniesPage() {
   return (
     <>
       <Header showBackButton={false} />
-      <Link to={`/details/${stocks[0][0].symbol}`}>
-        <div className="main-comp-section">
-          <span className="fa-solid fa-money-bill-trend-up" />
-          <div className="main-comp-name">
-            <h2>{stocks[0][0].symbol}</h2>
-            <span>
-              {stocks[0][0].revenue}
-              /
-              Revenue
-            </span>
-          </div>
-        </div>
-      </Link>
-      <div className="main-page-bar">
-        <p>US STOCK MARKET</p>
-      </div>
       <CompaniesList props={stocks} />
     </>
   );
